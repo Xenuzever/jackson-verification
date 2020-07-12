959f00497fe36288212ed21f3f839091544aedb6
kakeru.k <kakl2621@gmail.com>
2020-07-12 15:51:44 +0900

    Fix attribute.

--- src/main/java/org/example/model/Root.java
+++ src/main/java/org/example/model/Root.java
@@ -1,11 +1,13 @@
 package org.example.model;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.Data;
 
 @Data
 public class Root {
 
+  @JacksonXmlProperty(isAttribute = true)
   private String version;
 
   @JacksonXmlElementWrapper
