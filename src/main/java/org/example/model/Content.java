8776486725231c8d98cbd608b2d326e733fd4730
kakeru.k <kakl2621@gmail.com>
2020-07-12 16:54:56 +0900

    Fix add item.

--- src/main/java/org/example/model/Content.java
+++ src/main/java/org/example/model/Content.java
@@ -1,6 +1,5 @@
 package org.example.model;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
@@ -12,8 +11,7 @@ public class Content {
 
   private String description;
 
-  @JacksonXmlElementWrapper(useWrapping = false)
-  static final List<Item> items = new ArrayList<>();
+  private List<Item> items = new ArrayList<>();
 
   public Content() {
     items.clear();
@@ -21,9 +19,5 @@ public class Content {
 
   public void setItem(Item item) {
     items.add(item);
-  }
-
-  public List<Item> getItems() {
-    return items;
   }
 }
